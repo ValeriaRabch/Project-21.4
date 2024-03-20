5d9d5509756d81a37857dfec835df671bd05ad02
ValeriaRabch <rabchenukv@gmail.com>
2024-03-20 21:53:38 +0200

    Add func LettersAndNambers

--- Source.cpp
+++ Source.cpp
@@ -2,7 +2,23 @@
 #include<iostream>
 using namespace std;
 
-
+void LettersAndNambers(char* link1, char* link2, FILE* f) {
+	char c = getc(f);
+	FILE* f1, * f2;
+	f1 = fopen(link1, "w");
+	f2 = fopen(link2, "w");
+	while (c != EOF) {
+		if (c >= 'A' && c <= 'Z' || c >= 'a' && c <= 'z') {
+			fprintf(f1, "%c", c);
+		}
+		if (c >= '0' && c <= '9') {
+			fprintf(f2, "%c", c);
+		}
+		c = getc(f);
+	}
+	fclose(f1);
+	fclose(f2);
+}
 
 int main() {
 	FILE* f;
