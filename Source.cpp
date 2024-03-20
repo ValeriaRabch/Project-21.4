d74076d0a12bf6cb0acc7f168745ae46ea4eb4fe
ValeriaRabch <rabchenukv@gmail.com>
2024-03-20 21:53:14 +0200

    Create files

--- Source.cpp
+++ Source.cpp
@@ -5,6 +5,13 @@ using namespace std;
 
 
 int main() {
-	
+	FILE* f;
+	f = fopen("d:\\Valeria\\F.txt", "r");
+	char link1[] = { "d:\\Valeria\\F1.txt" };
+	char link2[] = { "d:\\Valeria\\F2.txt" };
+
+	LettersAndNambers(link1, link2, f);
+
+	fclose(f);
 	return 0;
 }
